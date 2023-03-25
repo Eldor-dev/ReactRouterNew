03f7d9b5f128cf5e5b2a5596859c245c224649dd
Eldor Azimov <eldorazimov37@gmail.com>
2023-03-25 17:53:09 +0900

    Layouts & Nested Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  createRoutesFromElements,
-  Route,
+  // createRoutesFromElements,
+  // Route,
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 // const routeDefinition = createRoutesFromElements(
 //   <Route>
@@ -15,8 +16,14 @@ import ProductsPage from "./pages/Products";
 // );
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 // const router = createBrowserRouter(routeDefinition);
---
+++ src/components/MainNavigation.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li className={classes.list}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={classes.list}>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
