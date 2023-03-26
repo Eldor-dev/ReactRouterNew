6e93c6a12780e0c42f29e76104422ce532c9a11d
Eldor Azimov <eldorazimov37@gmail.com>
2023-03-26 09:35:38 +0900

    Dynamic Routes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {
   // createRoutesFromElements,
   // Route,
 } from "react-router-dom";
+
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 // const routeDefinition = createRoutesFromElements(
 //   <Route>
@@ -24,6 +26,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
